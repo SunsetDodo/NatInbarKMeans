79bb4f9e725c2b4e62447b475b7cfe38475e5b8a
Inbar Polad <inbarpolad@mail.tau.ac.il>
2026-01-12 20:57:21 +0200

    added kmeans_pp.py

--- src/kmeans_pp.py
+++ src/kmeans_pp.py
@@ -36,51 +36,6 @@ def parse_file(file1, file2):
 
 
 
-
-# def main():
-#     if len(sys.argv) not in [5,6]:
-#         print("An Error Has Occurred")
-#         sys.exit(1)
-#
-#     k_raw = sys.argv[1]
-#     if not k_raw.replace('.', '').isnumeric() or k_raw.count('.') > 1 or k_raw.endswith('.') or int(float(k_raw)) != float(k_raw):
-#         print("Incorrect number of clusters!")
-#         sys.exit(1)
-#
-#     k = int(float(k_raw))
-#
-#     max_iters = 400
-#     if len(sys.argv) == 5:
-#         max_iters_raw = sys.argv[2]
-#         if not max_iters_raw.replace('.', '').isnumeric() or max_iters_raw.count('.') > 1 or max_iters_raw.endswith('.') or int(float(max_iters_raw)) != float(max_iters_raw):
-#             print("Incorrect maximum iteration!")
-#             sys.exit(1)
-#
-#         max_iters = int(float(max_iters_raw))
-#         file1 = sys.argv[4]
-#         file2 = sys.argv[5]
-#     else:
-#         file1 = sys.argv[3]
-#         file2 = sys.argv[4]
-#
-#
-#
-#     if not (1 < max_iters < 800):
-#         print("Incorrect maximum iteration!")
-#         sys.exit(1)
-#
-#
-#     parse_file(file1, file2)
-#     #
-#     # if not (1 < k < len(matrix)):
-#     #     print("Incorrect number of clusters!")
-#     #     sys.exit(1)
-#     #
-#     # centroids = kmeans(k, max_iters, matrix)
-#     # for centroid in centroids:
-#     #     print(centroid.center)
-
-
 def main():
     k = 5
     file1 = r"C:\Users\inbar\OneDrive\Documents\degree\Project_software\HW1\NatInbarKMeans\src\tests_ex2\input_1_db_1.txt"
